e578c0c5625a356c3fbcbd33f344da00be669d30
Muthoni_Macharia <paullymuthoni@gmail.com>
2022-09-21 21:15:17 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# Creates a file in /tmp
+# Create a file in tmp with basic requirements
 
 file { '/tmp/school':
   ensure  => file,
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
+  owner   => www-data,
+  group   => www-data,
   content => 'I love Puppet'
 }
